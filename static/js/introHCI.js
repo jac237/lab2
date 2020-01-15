de047d2a2647e0ddfcf69546f07c020768bced16
Jessie Cruz <jac237@ucsd.edu>
2020-01-15 00:16:02 -0800

    added click listener function inside intializePage().

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -6,4 +6,6 @@ function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+    
+    $("a.thumbnail").click(projectClick);
 }

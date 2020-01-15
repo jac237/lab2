a55af89fd63000d25c0e3d23c423e84e54035e1d
Jessie Cruz <jac237@ucsd.edu>
2020-01-15 00:19:20 -0800

    defined projectClick function in introHCI.js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,3 +9,11 @@ function initializePage() {
     
     $("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
